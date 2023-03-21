38c84e5c8574810006ecbb366ca5d0166787283c
vishalsiripuram <66208358+vishalsiripuram@users.noreply.github.com>
2023-03-21 12:19:56 +0530

    Update SpringbootApplication.java

--- src/main/java/Javaapp/springboot/SpringbootApplication.java
+++ src/main/java/Javaapp/springboot/SpringbootApplication.java
@@ -6,15 +6,11 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootApplication extends SpringBootServletInitializer {
+public class SpringbootApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(SpringApplication.class);
-	}
 
 }
